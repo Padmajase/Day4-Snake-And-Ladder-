510b7127cf6576f3724426a38e6ae9e0a302d06f
Padmajase <padmajapawar7@gmail.com>
2022-04-11 13:06:36 -0700

    Added : Snake and Ladder UC1

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+class SnakeAndLadder
+{
+	public static void main(String args[]) 
+	{
+	
+	System.out.println("Start position of player is at 0 ");
+	int position = 0;
+
+	}
+}
