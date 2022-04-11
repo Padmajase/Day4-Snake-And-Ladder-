102aa805aee7cad426ac40ede414aa57c1620873
Padmajase <padmajapawar7@gmail.com>
2022-04-11 14:21:23 -0700

    Added : Snake and Ladder UC4

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,30 +9,33 @@ class SnakeAndLadder
 	public static void main(String args[]) 
 	{
 	
-	System.out.println("Start position of player is  0 ");
-	int currentPosition = 0;
+		System.out.println("Start position of player is  0 ");
+		int currentPosition = 0;
 
-	Random rand = new Random();
-	int nextPosition = rand.nextInt(6) + 1;
+		while (currentPosition != 100)
+		{	
+			Random rand = new Random();
+			int nextPosition = rand.nextInt(6) + 1;
 	
-	System.out.println("after rolling die, number is  " +nextPosition);
+			System.out.println("after rolling die, number is  " +nextPosition);
 
-	if(nextPosition == ladder)
-	{
+			if(nextPosition == ladder)
+			{
 		
-		System.out.println("player moves ahead by  " + nextPosition + "number");
-		int currentPosition+ = nextPosition;	
-	}
-	else if(nextPosition == snake)
-	{	
-		System.out.println("player moves behind by  " + nextPosition + "number");
-		int currentPosition- = nextPosition;
-	}
-	else
-	{
-		System.out.println("player stays in same  position i.e. at" + currentPosition);
-		int currentPosition = currentposition;		
-	}
+				System.out.println("player moves ahead by  " + nextPosition + "number");
+				int currentPosition+ = nextPosition;	
+			}
+			else if(nextPosition == snake)
+			{	
+				System.out.println("player moves behind by  " + nextPosition + "number");
+				int currentPosition- = nextPosition;
+			}
+			else
+			{
+				System.out.println("player stays in same  position i.e. at" + currentPosition);
+				int currentPosition = currentposition;		
+			}
+		}
 	
 	}
 }
