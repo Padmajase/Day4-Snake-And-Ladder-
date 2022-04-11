dfe6597500ba007d4836676ac11b5379b0ce38b5
Padmajase <padmajapawar7@gmail.com>
2022-04-11 14:45:36 -0700

    Added : Snake and Ladder UC6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -11,31 +11,39 @@ class SnakeAndLadder
 	
 		System.out.println("Start position of player is  0 ");
 		int currentPosition = 0;
+		int count = 0;
+
+		
+		Random rand = new Random();
 
 		do
 		{	
-			Random rand = new Random();
 			int nextPosition = rand.nextInt(6) + 1;
+			count ++;
 	
 			System.out.println("after rolling die, number is  " +nextPosition);
 
 			if(nextPosition == ladder)
-			{
-		
+			{		
 				System.out.println("player moves ahead by  " + nextPosition + "number");
-				int currentPosition+ = nextPosition;	
+				int currentPosition+ = nextPosition;
+				System.out.println("current position of player is  " + currentPosition);	
 			}
 			else if(nextPosition == snake)
 			{	
 				System.out.println("player moves behind by  " + nextPosition + "number");
 				int currentPosition- = nextPosition;
+				System.out.println("current position of player is  " + currentPosition);
 			}
 			else
 			{
 				System.out.println("player stays in same  position i.e. at" + currentPosition);
-				int currentPosition = currentposition;		
+				int currentPosition = currentposition;
+				System.out.println("current position of player is  " + currentPosition);		
 			}
-		}while (currentPosition <= 100)
+		}while (currentPosition <= 100);
+
+		System.out.println("player plays dice   " + count + "  times to Win");
 	
 	}
 }
