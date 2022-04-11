6fccbfe1596a8fbfc67911256d4506ee2b16bd5b
Padmajase <padmajapawar7@gmail.com>
2022-04-11 14:15:37 -0700

    Added : Snake and Ladder UC3

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,6 @@
-// The Player rolls the die to get a number
-//between 1 to 6. - Use ((RANDOM)) to get the number between
-//1 to 6
+// The Player then checks for
+ // a Option. They are No Play,
+// Ladder or Snake. - Use ((RANDOM)) to check for Options
 
 import java.util.Random;
 
@@ -10,12 +10,29 @@ class SnakeAndLadder
 	{
 	
 	System.out.println("Start position of player is  0 ");
-	int position = 0;
+	int currentPosition = 0;
 
 	Random rand = new Random();
 	int nextPosition = rand.nextInt(6) + 1;
 	
 	System.out.println("after rolling die, number is  " +nextPosition);
 
+	if(nextPosition == ladder)
+	{
+		
+		System.out.println("player moves ahead by  " + nextPosition + "number");
+		int currentPosition+ = nextPosition;	
+	}
+	else if(nextPosition == snake)
+	{	
+		System.out.println("player moves behind by  " + nextPosition + "number");
+		int currentPosition- = nextPosition;
+	}
+	else
+	{
+		System.out.println("player stays in same  position i.e. at" + currentPosition);
+		int currentPosition = currentposition;		
+	}
+	
 	}
 }
